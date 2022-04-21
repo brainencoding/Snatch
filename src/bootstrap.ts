5797d07249882a27cbe8448188e833d97e110090
Adamets Vladislav <v.adamets@consult-info.ru>
2022-04-22 00:31:59 +0200

    ...

--- src/bootstrap.ts
+++ src/bootstrap.ts
@@ -1,4 +1,23 @@
+import {Snatch, SnatchCore} from "./core/Core.ts";
+
+const SnatchObject: object = Object.create({
+  Create: Snatch,
+  observePage: SnatchCore.observePage,
+
+  __core: {
+    SnatchCore,
+    Snatch
+  },
+});
+
 export function bootstrap(): void
 {
+  window.Snatch = SnatchObject;
+}
 
+declare global {
+  interface Window { Snatch: any; }
+  interface HTMLInputElement {
+    snatch: Snatch;
+  }
 }
--- src/core/Core.ts
+++ src/core/Core.ts
@@ -1,7 +1,8 @@
+import {v4} from "uuid";
+
 export interface ISnatch
 {
   setMasking(): Snatch;
-
   setValidating(): Snatch;
 }
 
@@ -10,21 +11,68 @@ type TOptions = {};
 export class Snatch implements ISnatch
 {
   private _input: HTMLInputElement;
+  private isMasking: boolean = false;
+  private isValidating: boolean = false;
+  public id: string = v4();
 
-  constructor(input: HTMLInputElement, options: TOptions)
+  constructor(input: HTMLInputElement, options?: TOptions)
   {
     this._input = input;
   }
 
   setMasking(): Snatch
   {
+    if (this.isMasking) {
+      return this;
+    }
 
 
+    this.isMasking = true;
     return this;
   }
 
   setValidating(): Snatch
   {
+    if (this.isValidating) {
+      return this;
+    }
+
+    this.isValidating = true;
     return this;
+  }
+}
+
+export class SnatchCore
+{
+  static currentObserver: MutationObserver;
+
+  public static observePage(): void
+  {
+    this.currentObserver = new MutationObserver(SnatchCore.observerPageHandler);
+    this.currentObserver.observe(
+      document.body,
+      {
+        attributes: true,
+        subtree: true,
+        childList: true,
+      }
+    );
+  }
+
+  private static observerPageHandler(): void
+  {
+    const allInputs: NodeListOf<HTMLInputElement> = document.querySelectorAll('input[type="tel"]');
+
+    if (allInputs.length) {
+      for (let i = 0; i < allInputs.length; i++) {
+        const currentInput = allInputs[i];
+
+        if (currentInput.hasOwnProperty('snatch')) {
+          return;
+        }
+
+        currentInput.snatch = new Snatch(currentInput);
+      }
+    }
   }
 }
